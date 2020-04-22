d75de3423a008839f1c06b173fd3c4866d3c9165
Kiều Phước Thiện <thien.dpit@gmail.com>
2020-04-22 08:07:06 +0700

    project chapter 3 function

---
+++ Python_Basics/functions/collatz.py
@@ -0,0 +1,16 @@
+def collatz(number):
+  return number // 2 if number % 2 == 0 else number * 3 + 1
+
+if __name__ == "__main__":
+  print('Enter number:')
+  try:
+    number = int(input())
+    while True:
+      if number != 1:
+        number = collatz(number)
+        print(number)
+      else:
+        break
+  except:
+    print('Error: Invalid Value. You did not enter an interger.')
+  
